d440f2632e3a90cc34ff7bcd04b46c6bb5f9534d
Linda Zhou <linda.zhouyq@gmail.com>
2016-05-11 04:52:07 +0000

    I'm done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all # Get's all the Users in our database and assigns it the the instance variable @user.
+
   end
 
   def new
     @user = User.new #Creates an empty user object.
+
   end
 
   def create
@@ -16,15 +18,18 @@ class UsersController < ApplicationController
     else
       render "new" #Go to the new view for the user
     end
+
   end
 
   def show
     @user = User.find(params[:id]) # Query for the user with the id that is in params
+
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email) #Returns a hash that was the value of "name" and "email" from the value of "user" in params.
+
   end
 end
